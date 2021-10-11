e326dd26eff27f14fb4bc6adba543f78fb98d5c5
shohirev <vishohirev@yandex.ru>
2021-10-11 11:23:43 +0500

    Revert "passing test"
    This reverts commit 85312cbe6e18129d52b870b3936f230354ac0c96.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,25 @@
 import 'core-js/stable/index.js';
 import 'regenerator-runtime/runtime.js';
 import '../assets/application.scss';
-import { ReactDOM } from 'react';
-import App from './App.jsx';
+import React, { Suspense, useState } from 'react';
+import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from 'react-router-dom';
+import './i18n.js';
+import { store } from './app/store.js';
+import { Provider } from 'react-redux';
+import { Container } from 'react-bootstrap';
+import Main from './Main.jsx';
+import AuthPage from './AuthPage.jsx';
+import SignUp from './SignUp.jsx';
+import NotFoundPage from './NotFoundPage.jsx';
+import authContext from './contexts/index.jsx';
+import useAuth from './hooks/index.jsx';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
@@ -19,14 +36,72 @@ const rollbarConfig = {
   }
 };
 
-const init = async () => {
-  const appContainer = document.querySelector('#chat');
-  await ReactDOM.render(
-    <App />,
-    appContainer
+const AuthProvider = ({ children }) => {
+  const isAuthorized = localStorage.getItem('token');
+  const [ loggedIn, setLoggedIn ] = useState(isAuthorized);
+  const logIn = (token) => {
+    localStorage.setItem('token', token);
+    setLoggedIn(true);
+  };
+  const logOut = () => {
+    localStorage.removeItem('token');
+    setLoggedIn(false);
+  };
+
+  return (
+    <authContext.Provider value={{ loggedIn, logIn, logOut }}>
+      {children}
+    </authContext.Provider>
   );
 };
 
-export default init();
+const LoggedInRoute = ({ children }) => {
+  const auth = useAuth();
 
-init();
+  return (
+    <Route
+      render={() => 
+        auth.loggedIn ? (
+          children
+        ) :
+        (
+          <Redirect to='/login' />
+        )
+      }
+    >
+    </Route>
+  );
+};
+
+const Chat = () => {
+  return (
+    <AuthProvider>
+      <Router>
+        <Switch>
+          <LoggedInRoute exact path='/'>
+            <Main />
+          </LoggedInRoute>
+          <Route exact path='/login'>
+            <AuthPage />
+          </Route>
+          <Route exact path='/signup'>
+            <SignUp />
+          </Route>
+          <Route path='*'>
+            <NotFoundPage />
+          </Route>
+        </Switch>
+      </Router>
+    </AuthProvider>
+  );
+};
+
+const appContainer = document.querySelector('#chat');
+ReactDOM.render(
+  <Suspense fallback="loading">
+    <Provider store={store}>
+      <Chat />
+    </Provider>
+  </Suspense>,
+  appContainer
+);
