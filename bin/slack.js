7e781dd9a4aa7c73aa56655115afa0f33d6c45f2
shohirev <vishohirev@yandex.ru>
2021-10-23 11:18:38 +0500

    making appropriate layout

--- bin/slack.js
+++ bin/slack.js
@@ -1,9 +1,9 @@
 #! /usr/bin/env node
 
-import getApp from "../server/index.js";
+import getApp from '../server/index.js';
 
 const port = process.env.PORT || 5000;
-const address = "0.0.0.0";
+const address = '0.0.0.0';
 
 getApp({ port }).then((app) => {
   app.listen(port, address, () => {
